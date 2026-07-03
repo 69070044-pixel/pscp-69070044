0d3a28731235f4c6abf2459fa01216917ce80dd6
it69070247 <69070247@kmitl.ac.th>
2026-07-03 15:08:20 +0700

    new commit

--- oj2998/main.py
+++ oj2998/main.py
@@ -1,1 +1,10 @@
 """OJ2998"""
+import math as m
+
+q1 = float(input())
+q2 = float(input())
+p1 = float(input())
+p2 = float(input())
+
+result = (m.sqrt(m.pow(p1 - q1, 2) + m.pow(p2 - q2, 2)))
+print(result)
