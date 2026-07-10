a9df2f141ff275f2f6115b5161b7c3e78bd57eda
NPK-IT69070044 <69070044@kmitl.ac.th>
2026-07-10 23:27:25 +0700

    new commit

--- work/oj3023_work/main.py
+++ work/oj3023_work/main.py
@@ -1,2 +1,18 @@
 """OJ3023"""
 n = int(input())
+if n == 1:
+    print(1)
+else:
+    LEN_OF_NUM = len(str(n)) + 1
+    constant = 0
+    if LEN_OF_NUM == 3:
+        constant = -9
+    elif LEN_OF_NUM == 4:
+        constant = -108
+    elif LEN_OF_NUM == 5:
+        constant = -1107
+    elif LEN_OF_NUM == 6:
+        constant = -11106
+    elif LEN_OF_NUM == 7:
+        constant = -111105
+    print((LEN_OF_NUM*n) + constant)
