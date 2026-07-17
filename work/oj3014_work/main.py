4de110ae29ae95c7d1defc664c2924d34d861474
69070044-pixel <69070044@kmitl.ac.th>
2026-07-17 13:25:53 +0700

    fix oj3014

--- work/oj3014_work/main.py
+++ work/oj3014_work/main.py
@@ -8,6 +8,4 @@ budget = float(input())
 if not cap_req:
     print(budget // milk_price)
 else:
-    get_milk = budget // milk_price
-    promo = get_milk // cap_req
-    print(int(get_milk + (promo * milk_promo)))
+    pass
