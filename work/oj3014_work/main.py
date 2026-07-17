a5f0aa5622937003b7fc8dd78881ad13527b27a5
69070044-pixel <69070044@kmitl.ac.th>
2026-07-17 23:25:18 +0700

    update

--- work/oj3014_work/main.py
+++ work/oj3014_work/main.py
@@ -2,10 +2,18 @@
 
 milk_price = float(input())
 cap_req = int(input())
-milk_promo = int(input())
+milk_free = int(input())
 budget = float(input())
 
 if not cap_req:
-    print(budget // milk_price)
+    print(int(budget // milk_price))
 else:
-    pass
+    milk = int(budget // milk_price)
+    cap = milk
+
+    while cap >= cap_req:
+        cap -= cap_req
+        milk += milk_free
+        cap += milk_free
+
+    print(milk)
