d0f378fddb3dddb97160bf4cadece9af16fd1c0b
nopphon-k23251 <nopphon052k@gmail.com>
2026-07-20 15:44:45 +0700

    comment in oj3031

--- work/11-20/oj3010_work/main.py
+++ work/11-20/oj3010_work/main.py
@@ -6,12 +6,6 @@ if (x, y) == (0, 0):
 elif not x or not y:
     print("Y" if not x else "X")
 elif x > 0:
-    if y > 0:
-        print("Q1")
-    else:
-        print("Q4")
+    print("Q1" if y > 0 else "Q4")
 elif x < 0:
-    if y > 0:
-        print("Q2")
-    else:
-        print("Q3")
+    print("Q2" if y > 0 else "Q3")
--- work/21-30/oj3031_work/main.py
+++ work/21-30/oj3031_work/main.py
@@ -7,10 +7,12 @@ while n:
     n -= 1
 
 for pos in position:
-    x, y = pos
-    r_pow2 = x ** 2 + y ** 2
-    current_area = PI * r_pow2
+    x, y = pos #แจกพิกัดให้ x, y
+    r_pow2 = x ** 2 + y ** 2 # หาค่า r^2
+    current_area = PI * r_pow2 # พื้นที่ของ Ink เมื่อขยายไปถึงพิกัด x, y
     if not current_area % S:
-        print(int(current_area / S))
+        # ไม่มีเศษ
+        print(int(current_area / S)) # area / rate = time
     else:
+        # มีเศษ
         print(int(current_area / S) + 1)
