aecdb682dcbf9ad1914c968f01f3efb14543ba74
nopphon-k23251 <nopphon052k@gmail.com>
2026-07-26 19:48:52 +0700

    oj3058 rework

--- work/3042-3066/oj3058_work/main.py
+++ work/3042-3066/oj3058_work/main.py
@@ -3,17 +3,13 @@ small_brick = int(input())
 big_brick = int(input())
 goal = int(input())
 
-need_big_brick = goal // 5 # หาจำนวนก้อนให้ที่มาที่สุดที่ต้องใช้
-if need_big_brick <= big_brick:
+big_use = goal // 5 # หาจำนวนก้อนให้ที่มาที่สุดที่ต้องใช้
+if big_use <= big_brick:
     # จำนวนที่ต้องการก้อนใหญ่ที่ใช้มีพอ
-    small_brick_use = goal - (need_big_brick * 5) # เอาก้อนใหญ่แค่ที่ใช้ไป * 5 เพื่อหาระยะที่ต่อได้
+    small_use = goal - (big_use * 5) # เอาก้อนใหญ่แค่ที่ใช้ไป * 5 เพื่อหาระยะที่ต่อได้
 else:
     # ก้อนใหญ่ใช้หมดแล้วไม่พอต้องต่อก้อนเล็กเพิ่ม
-    small_brick_use = goal - (big_brick * 5) # เอาก้อนใหญ่ทั้งหมดใช้ไป * 5 เพื่อหาระยะที่ต่อได้
+    small_use = goal - (big_brick * 5) # เอาก้อนใหญ่ทั้งหมดใช้ไป * 5 เพื่อหาระยะที่ต่อได้
 
-if small_brick >= small_brick_use:
-    #  check ว่าก่อนเล็กมีพอมั้ย
-    print(small_brick_use)
-else:
-    # ไม่พอต่อไม่ได้
-    print(-1)
+#  check ว่าก่อนเล็กมีพอมั้ย ถ้าไม่พอ print -1
+print(small_use if small_brick >= small_use else -1)
